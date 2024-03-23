2189c202260c9cebd9c67892dd3c94e76c3b9d83
Charlie Willoughby <104572309+CWilloughby98@users.noreply.github.com>
2024-03-23 11:14:23 +0000

    Domain-Generator-Done

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".com", ".net", ".org", ".co", ".us"];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const getRandomDomain = (arr, arr2, arr3, arr4) => {
+    let randomDomain = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr2.length; j++) {
+        for (let k = 0; k < arr3.length; k++) {
+          for (let l = 0; l < arr4.length; l++) {
+            randomDomain.push(arr[i] + arr2[j] + arr3[k] + arr4[l]);
+          }
+        }
+      }
+    }
+    return randomDomain;
+  };
+  let domain = getRandomDomain(pronoun, adj, noun, extension);
+
+  for (let iteration of domain) {
+    console.log(iteration);
+  }
+  // console.log(getRandomDomain(pronoun, adj, noun, extension));
 };
