dcca02e96a7789a99bfd503dda85bf94bea45a9e
Charlie Willoughby <104572309+CWilloughby98@users.noreply.github.com>
2024-03-26 19:57:34 +0000

    param-name-change

--- src/app.js
+++ src/app.js
@@ -8,15 +8,16 @@ let noun = ["jogger", "racoon"];
 let extension = [".com", ".net", ".org", ".co", ".us"];
 
 window.onload = function() {
-  //write your code here
-  const getRandomDomain = (arr, arr2, arr3, arr4) => {
+  
+  //re-write paremeter names to understand the order better
+  const getRandomDomain = (pronoun, adj, noun, extension) => {
     let randomDomain = [];
 
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0; j < arr2.length; j++) {
-        for (let k = 0; k < arr3.length; k++) {
-          for (let l = 0; l < arr4.length; l++) {
-            randomDomain.push(arr[i] + arr2[j] + arr3[k] + arr4[l]);
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let l = 0; l < extension.length; l++) {
+            randomDomain.push(pronoun[i] + adj[j] + noun[k] + extension[l]);
           }
         }
       }
